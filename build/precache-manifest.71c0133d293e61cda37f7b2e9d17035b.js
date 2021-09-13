7631742b115dcb7408923cc41a2418144b564439
hardcoresmp <ikshit.naman@gmail.com>
2021-09-13 12:29:20 +0530

    g

---
+++ build/precache-manifest.71c0133d293e61cda37f7b2e9d17035b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a3ac4fcd683ad2312036f7fde14a89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ebf20563e7d7a18d444",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9ebf20563e7d7a18d444",
+    "url": "/static/js/2.b1ed7d04.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/static/js/2.b1ed7d04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa5cf88a4d2a65413a0",
+    "url": "/static/js/main.0bfe9407.chunk.js"
+  },
+  {
+    "revision": "992c1d3a7787c5ab29a5",
+    "url": "/static/js/runtime-main.eeaa320a.js"
+  }
+]);
